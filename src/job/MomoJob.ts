a8d160fb229327d13c5e1fd719b84fb3c9cf27cd
Ute Weiss <ute.weiss@tngtech.com>
2022-06-24 10:57:19 +0200

    refactor: simplify MomoJob
    Signed-off-by: Ute Weiss <ute.weiss@tngtech.com>

--- src/job/MomoJob.ts
+++ src/job/MomoJob.ts
@@ -1,8 +1,9 @@
 export type Handler = () => Promise<string | undefined | void> | string | undefined | void;
 
-interface MomoBaseJob {
+export interface MomoJob {
   handler: Handler;
   name: string;
+  schedule: Interval | CronSchedule;
   concurrency?: number;
   maxRunning?: number;
 }
@@ -16,38 +17,17 @@ export interface CronSchedule {
   cronSchedule: string;
 }
 
-export function isInterval(input: unknown): input is Interval {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isInterval(input: any): input is Interval {
   return (
-    // @ts-ignore
     input.interval !== undefined &&
-    // @ts-ignore
     input.firstRunAfter !== undefined &&
-    typeof (input as Interval).interval == 'string' &&
-    typeof (input as Interval).firstRunAfter == 'number'
+    typeof input.interval == 'string' &&
+    typeof input.firstRunAfter == 'number'
   );
 }
 
-export function isCronSchedule(input: unknown): input is CronSchedule {
-  return (
-    // @ts-ignore
-    input.cronSchedule !== undefined && typeof (input as CronSchedule).cronSchedule == 'string'
-  );
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isCronSchedule(input: any): input is CronSchedule {
+  return input.cronSchedule !== undefined && typeof input.cronSchedule == 'string';
 }
-
-export interface MomoIntervalJob extends MomoBaseJob {
-  schedule: Interval;
-}
-
-export function isMomoIntervalJob(input: unknown): input is MomoIntervalJob {
-  return isInterval((input as MomoIntervalJob).schedule);
-}
-
-export interface MomoCronJob extends MomoBaseJob {
-  schedule: CronSchedule;
-}
-
-export function isMomoCronJob(input: MomoJob): input is MomoCronJob {
-  return isCronSchedule((input as MomoIntervalJob).schedule);
-}
-
-export type MomoJob = MomoCronJob | MomoIntervalJob;
